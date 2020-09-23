3a12e2ce34770c14ac986e14328d01e017719023
Mario Henrique Monteiro de Albuquerque <brgamer3321@gmail.com>
2020-09-23 12:01:22 -0300

    Adicionado json em Professor

---
+++ lib/professor.dart
@@ -0,0 +1,193 @@
+import 'package:dsi_app/constants.dart';
+import 'package:dsi_app/dsi_widgets.dart';
+import 'package:dsi_app/infra.dart';
+import 'package:dsi_app/pessoa.dart';
+import 'package:flutter/material.dart';
+
+
+class Professor extends Pessoa {
+
+  String idprof;
+
+  //Construtor da classe Professor.
+  Professor({cpf, nome, endereco, this.idprof})
+      : super(cpf: cpf, nome: nome, endereco: endereco);
+
+  Professor.fromJson(Map<String, dynamic> json)
+      : idprof = json['codID'],
+        super.fromJson(json);
+
+  ///TIP este método converte o objeto atual para um mapa que representa um
+  ///objeto JSON.
+  Map<String, dynamic> toJson() => super.toJson()
+    ..addAll({
+      'codID': idprof,
+    });
+
+}
+var professorController = ProfessorController();
+
+class ProfessorController {
+  List<Professor> getAll() {
+    return pessoaController.getAll().whereType<Professor>().toList();
+  }
+
+  Professor save(professor) {
+    return pessoaController.save(professor);
+  }
+
+  bool remove(professor) {
+    return pessoaController.remove(professor);
+  }
+}
+
+class ListProfessorPage extends StatefulWidget {
+  @override
+  ListProfessorPageState createState() => ListProfessorPageState();
+}
+
+class ListProfessorPageState extends State<ListProfessorPage> {
+  List<Professor> _professores = professorController.getAll();
+
+  @override
+  Widget build(BuildContext context) {
+    return DsiScaffold(
+      title: 'Listagem de Professores',
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => dsiHelper.go(context, '/maintain_professor'),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        // physics: NeverScrollableScrollPhysics(),
+        itemCount: _professores.length,
+        itemBuilder: _buildListTileProfessor,
+      ),
+    );
+  }
+
+  Widget _buildListTileProfessor(context, index) {
+    var professor = _professores[index];
+    print(professor);
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        setState(() {
+          professorController.remove(professor);
+          _professores.remove(index);
+        });
+        dsiHelper.showMessage(
+          context: context,
+          message: '${professor.nome} foi removido.',
+        );
+      },
+      background: Container(
+        color: Colors.red,
+        child: Row(
+          children: <Widget>[
+            Constants.boxSmallHeight,
+            Icon(Icons.delete, color: Colors.white),
+            Spacer(),
+            Icon(Icons.delete, color: Colors.white),
+            Constants.boxSmallHeight,
+          ],
+        ),
+      ),
+      child: ListTile(
+        title: Text(professor.nome),
+        subtitle: Column(
+          children: <Widget>[
+            SizedBox(width: 8.0),
+            Text('IDprof. ${professor.idprof}'),
+          ],
+        ),
+        onTap: () => dsiHelper.go(context, "/maintain_professor", arguments: professor),
+      ),
+    );
+  }
+}
+
+class MaintainProfessorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Professor professor = dsiHelper.getRouteArgs(context);
+    if (professor == null) {
+      professor = new Professor();
+    }
+
+    return DsiBasicFormPage(
+        title: 'Professor',
+        onSave: () {
+      professorController.save(professor);
+      dsiHelper.go(context, '/list_professor');
+    },
+    body: Wrap(
+    alignment: WrapAlignment.center,
+    runSpacing: Constants.boxSmallHeight.height,
+    children: <Widget>[
+    MaintainPessoaBody(professor),
+    TextFormField(
+    keyboardType: TextInputType.number,
+    decoration: const InputDecoration(labelText: 'codigo de ID*'),
+    validator: (String value) {
+    return value.isEmpty ? 'codigo de ID inválido.' : null;
+    },
+    initialValue: professor.idprof,
+    onSaved: (newValue) => professor.idprof = newValue,)]));
+
+
+    /*return DsiBasicFormPage(
+      title: 'Professor',
+      onSave: () {
+        professorController.save(professor);
+        dsiHelper.go(context, '/list_professor');
+      },
+      body: Wrap(
+        alignment: WrapAlignment.center,
+        runSpacing: Constants.boxSmallHeight.height,
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(labelText: 'CPF*'),
+            validator: (String value) {
+              return value.isEmpty ? 'CPF inválido.' : null;
+            },
+            initialValue: professor.cpf,
+            onSaved: (newValue) => professor.cpf = newValue,
+          ),
+          Constants.boxSmallHeight,
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(labelText: 'Nome*'),
+            validator: (String value) {
+              return value.isEmpty ? 'Nome inválido.' : null;
+            },
+            initialValue: professor.nome,
+            onSaved: (newValue) => professor.nome = newValue,
+          ),
+          Constants.boxSmallHeight,
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(labelText: 'Endereço*'),
+            validator: (String value) {
+              return value.isEmpty ? 'Endereço inválido.' : null;
+            },
+            initialValue: professor.endereco,
+            onSaved: (newValue) => professor.endereco = newValue,
+          ),
+          Constants.spaceSmallHeight,
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(labelText: 'IDprofessor*'),
+            validator: (String value) {
+              return value.isEmpty ? 'IDprofessor inválido.' : null;
+            },
+            initialValue: professor.idprof,
+            onSaved: (newValue) => professor.idprof = newValue,
+          ),
+        ],
+      ),
+    );*/
+  }
+}
